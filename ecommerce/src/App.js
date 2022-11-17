3b8af35086d2f46d63f243f277ac722fe43a13d7
Gözde <altuntasgozde@gmail.com>
2022-11-17 20:04:48 +0300

    strapi created

--- ecommerce/src/App.js
+++ ecommerce/src/App.js
@@ -1,11 +1,34 @@
-import './App.css';
+import { useEffect, useState } from "react";
+import { NavbarItem } from "./NavbarItem";
+import Card from "react-bootstrap/Card";
+import axios from "axios";
+import "./App.css";
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:1337/api/products")
+      .then((response) => setData(response.data.data));
+  }, []);
+
   return (
     <div className="App">
-      <div>
-        
-      </div>
+      <NavbarItem />
+      {data.map((item) => (
+        <div key={item.id}>
+          <Card style={{ width: "18rem" }}>
+            <Card.Body>
+              <Card.Title>{item.attributes.title}</Card.Title>
+              <Card.Subtitle className="mb-2 text-muted">
+                {item.attributes.price}
+              </Card.Subtitle>
+              <Card.Text>{item.attributes.features}</Card.Text>
+            </Card.Body>
+          </Card>
+        </div>
+      ))}
     </div>
   );
 }
